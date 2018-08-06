02fa19da1e36115240f8cdb2eb8663b7be1e2d3f
peterpeterp <peterpfleiderer@gmx.de>
2018-08-06 12:05:45 +0200

    nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'nbsphinx'
     # 'sphinx.ext.autosummary',
     # 'sphinxcontrib.fulltoc',
 ]
