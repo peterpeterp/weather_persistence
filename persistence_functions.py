1677451138093415ba2e237037d0244768b7a9e8
peterpeterp <peterpfleiderer@gmx.de>
2018-10-18 12:08:41 +0200

    new persistence function which is state specific

--- persistence_functions.py
+++ persistence_functions.py
@@ -47,7 +47,6 @@ def period_identifier(ind):
 
 	return(pers)
 
-
 def optimized_period_identifier(ind):
 	"""
 	This function identifies persistent periods using collections. It isn't a straight foreward implementation but runs faster than :meth:`period_identifier`
@@ -98,6 +97,43 @@ def optimized_period_identifier(ind):
 
 	return(pers)
 
+def optimized_period_identifier_1_state(ind,state):
+	"""
+	This function identifies persistent periods using collections. It isn't a straight foreward implementation but runs faster than :meth:`period_identifier`
+
+	Parameters
+	----------
+		ind: np.array
+			array containing of state indices
+
+		state: np.float
+			state index to be analyzed
+
+	Returns
+	--------
+		pers: np.array
+			array of the same length as `ind` containing the length of identified periods. Periods of state -1 are have negative lengths. The period length of a period is placed in the center of the period. All other values are 0.
+	"""
+	pers=ind.copy()*0
+
+	ind_tmp=ind.copy()
+	ind_tmp[ind_tmp!=state]=999
+	ind_tmp[ind_tmp==state]=0
+	ind_tmp[ind_tmp==999]=1
+
+	su=np.cumsum(ind_tmp)
+	counter=collections.Counter(su)
+
+	index=0
+	for count,val in zip(counter.values(),counter.keys()):
+		index+=count
+		if count>=1:
+			pers[index-(count-1)/2-1]=(count-1)
+	# correct start
+	if ind_tmp[0]==0 and ind_tmp[1]==1:	pers[0]=1
+	if ind_tmp[0]==0 and ind_tmp[1]==0:	pers[np.where(pers>0)[0][0]]+=1
+
+	return(pers)
 
 def test_persistence(N):
 	ind=np.random.random(N)
@@ -119,14 +155,20 @@ def test_persistence(N):
 	print("--- optimized_period_identifier %s seconds ---" % (time.time() - start_time))
 
 	start_time = time.time()
-	optimized_old=optimized_period_identifier_old(ind)
-	print(optimized_old[0:100])
-	print("--- optimized_period_identifier_old %s seconds ---" % (time.time() - start_time))
+	state1=optimized_period_identifier_1_state(ind,1)
+	state2=optimized_period_identifier_1_state(ind,-1)
+	combined = state1 - state2
+	print(optimized[0:100])
+	print("--- optimized_period_identifier %s seconds ---" % (time.time() - start_time))
 
+	# start_time = time.time()
+	# optimized_old=optimized_period_identifier_old(ind)
+	# print(optimized_old[0:100])
+	# print("--- optimized_period_identifier_old %s seconds ---" % (time.time() - start_time))
 
-#test_persistence(100)
+# test_persistence(100)
 
-def get_persistence(state_file,out_file, lat_name='lat', lon_name='lon', seasons={'MAM':{'months':[3,4,5],'index':0}, 'JJA':{'months':[6,7,8],'index':1}, 'SON':{'months':[9,10,11],'index':2}, 'DJF':{'months':[12,1,2],'index':3}},overwrite=True):
+def get_persistence(state_file,states_to_analyze={1:'warm',-1:'cold'}, lat_name='lat', lon_name='lon', seasons={'MAM':{'months':[3,4,5],'index':0}, 'JJA':{'months':[6,7,8],'index':1}, 'SON':{'months':[9,10,11],'index':2}, 'DJF':{'months':[12,1,2],'index':3}},overwrite=True):
 	"""
 	This function reads a state field created by :meth:`temp_anomaly_to_ind` or :meth:`precip_to_index` and finds persistent periods for these statesself. It uses :meth:`optimized_period_identifier`
 
@@ -142,13 +184,13 @@ def get_persistence(state_file,out_file, lat_name='lat', lon_name='lon', seasons
 			overwrites existing files
 	"""
 
-	nc_in=Dataset(state_file,'r')
+	nc_in=da.read_nc(state_file)
 	# handle time
-	time_axis=nc_in.variables['time'][:]
-	if 'calendar' in nc_in.variables['time'].ncattrs():
-		datevar = num2date(time_axis,units = nc_in.variables['time'].units,calendar = nc_in.variables['time'].calendar)
+	time_axis=nc_in['time'].values
+	if 'calendar' in nc_in['time'].attrs.keys():
+		datevar = num2date(time_axis,units = nc_in['time'].units,calendar = nc_in['time'].calendar)
 	else:
-		datevar = num2date(time_axis,units = nc_in.variables['time'].units)
+		datevar = num2date(time_axis,units = nc_in['time'].units)
 
 	month=np.array([int(str(date).split("-")[1])	for date in datevar[:]])
 	year=np.array([int(str(date).split("-")[0])	for date in datevar[:]])
@@ -160,82 +202,70 @@ def get_persistence(state_file,out_file, lat_name='lat', lon_name='lon', seasons
 	monthly_index=np.array([mon+yr*12 for mon,yr in zip(month-1,year-np.min(year))])
 	mon_year_axis=np.array([yr+mn*0.01 for yr,mn in zip(year,month)])
 
-	state=np.ma.getdata(nc_in.variables['state'][:].squeeze())
-	mask=np.ma.getmask(nc_in.variables['state'][:].squeeze())
+	state=np.ma.getdata(nc_in['state'].values.squeeze())
+	mask=np.ma.getmask(nc_in['state'].values.squeeze())
 	state[mask]=np.nan
-	print(state.shape)
-	print(np.nanpercentile(state,range(100)))
+	print('state percentiles: ',np.nanpercentile(state,range(100)))
 
 
-	#period=state.copy()*np.nan
-	period_length=state.copy()*np.nan
-	period_state=state.copy()*np.nan
-	period_midpoints=state.copy()*np.nan
-	period_season=state.copy()*np.nan
-	period_monthly_index=state.copy()*np.nan
-	gc.collect()
+	for state_id,state_name in states_to_analyze.items():
+		period_length=state.copy()*np.nan
+		period_midpoints=state.copy()*np.nan
+		period_season=state.copy()*np.nan
+		period_monthly_index=state.copy()*np.nan
+		gc.collect()
 
-	period_number=[]
-	print('finding periods\n10------50-------100')
-	for y,progress in zip(range(state.shape[1]), np.array([['-']+['']*(state.shape[1]/20+1)]*20).flatten()[0:state.shape[1]]):
-		sys.stdout.write(progress); sys.stdout.flush()
-		for x in range(state.shape[2]):
-			start_time=time.time()
-			if np.isfinite(np.nanmean(state[:,y,x])) and np.nanmean(state[:,y,x]) not in [-1,1]:
-				periods=optimized_period_identifier(state[:,y,x].copy())
-				identified_periods=np.where(periods!=0)[0]
-				per_num=len(identified_periods)
-				period_number.append(per_num)
+		period_number=[]
+		print('finding periods\n10------50-------100')
+		for y,progress in zip(range(state.shape[1]), np.array([['-']+['']*(state.shape[1]/20+1)]*20).flatten()[0:state.shape[1]]):
+			sys.stdout.write(progress); sys.stdout.flush()
+			for x in range(state.shape[2])[10:11]:
+				start_time=time.time()
+				if np.isfinite(np.nanmean(state[:,y,x])) and np.nanmean(state[:,y,x]) != state_id:
+					periods=optimized_period_identifier_1_state(state[:,y,x].copy(),state_id)
+					identified_periods=np.where(periods!=0)[0]
+					per_num=len(identified_periods)
+					period_number.append(per_num)
 
-				period_length[0:per_num,y,x]=periods[identified_periods]
-				period_state[0:per_num,y,x]=np.sign(periods[identified_periods])
-				period_midpoints[0:per_num,y,x]=time_axis[identified_periods]
-				period_season[0:per_num,y,x]=season[identified_periods]
-				period_monthly_index[0:per_num,y,x]=monthly_index[identified_periods]
-			gc.collect()
+					period_length[0:per_num,y,x]=periods[identified_periods]
+					period_midpoints[0:per_num,y,x]=time_axis[identified_periods]
+					period_season[0:per_num,y,x]=season[identified_periods]
+					period_monthly_index[0:per_num,y,x]=monthly_index[identified_periods]
+				gc.collect()
 
-	# if len(period_number)==0:
-	# 	return 'fail'
+		per_num=max(period_number)
+		ds_out = da.Dataset({})
 
-	per_num=max(period_number)
+		for name in ['lon','lat']:
+			tmp = nc_in[name]
+			for key,val in nc_in[name].attrs.items():
+				tmp.attrs[key] = val
+			ds_out[name] = tmp
 
-	if overwrite: os.system('rm '+out_file)
-	nc_out=Dataset(out_file,'w')
-	for dname, the_dim in nc_in.dimensions.iteritems():
-		if dname in [lon_name,lat_name]:nc_out.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
-	nc_out.createDimension('period_id', per_num)
+		ds_out['period_length'] = da.DimArray(period_length[0:per_num,:,:], axes=[np.asarray(range(per_num),dtype=np.dtype('i2')),nc_in['lat'].values,nc_in['lon'].values], dims=['period_id','lat','lon'], dtype=np.dtype('i2'))
+		ds_out['period_length'].units = 'days'
+		ds_out['period_length'].state_description=nc_in['state'].description
+		ds_out['period_length'].analyzed_states=str(states_to_analyze)
 
-	for v_name, varin in nc_in.variables.iteritems():
-		if v_name in [lon_name,lat_name]:
-			outVar = nc_out.createVariable(v_name, varin.datatype, varin.dimensions)
-			outVar.setncatts({k: varin.getncattr(k) for k in varin.ncattrs()})
-			outVar[:] = varin[:]
+		ds_out['period_midpoints'] = da.DimArray(period_midpoints[0:per_num,:,:], axes=[np.asarray(range(per_num),dtype=np.dtype('i2')),nc_in['lat'].values,nc_in['lon'].values], dims=['period_id','lat','lon'], dtype=np.dtype('f'))
+		ds_out['period_midpoints'].description = 'midpoint of period based on time axis in state-file'
+		ds_out['period_midpoints'].units = nc_in['time'].units
+		if 'calendar' in nc_in['time'].attrs.keys():
+			ds_out['period_midpoints'].calendar = nc_in['time'].calendar
 
-	outVar = nc_out.createVariable('period_length','i2',('period_id',lat_name,lon_name,))
-	outVar.long_name='period length in days'
-	outVar[:] = period_length[0:per_num,:,:]
+		ds_out['period_season'] = da.DimArray(period_season[0:per_num,:,:], axes=[np.asarray(range(per_num),dtype=np.dtype('i2')),nc_in['lat'].values,nc_in['lon'].values], dims=['period_id','lat','lon'], dtype=np.dtype('i1'))
+		ds_out['period_season'].description = str(seasons)
+		ds_out['period_season'].long_name = 'season in which the midpoint of period is located'
 
-	outVar = nc_out.createVariable('period_state','i1',('period_id',lat_name,lon_name,))
-	outVar.long_name='period type'
-	outVar[:] = period_state[0:per_num,:,:]
+		ds_out['period_monthly_index'] = da.DimArray(period_monthly_index[0:per_num,:,:], axes=[np.asarray(range(per_num),dtype=np.dtype('i2')),nc_in['lat'].values,nc_in['lon'].values], dims=['period_id','lat','lon'], dtype=np.dtype('i2'))
+		ds_out['period_monthly_index'].description = 'monthly index 0 to number of years * 12 (based on time axis of state-file)'
+		ds_out['period_monthly_index'].first_time_step = str(year[0])+' - '+str(min(month))
+		ds_out['period_monthly_index'].last_time_step = str(year[-1])+' - '+str(max(month))
 
-	outVar = nc_out.createVariable('period_midpoints','f',('period_id',lat_name,lon_name,))
-	outVar.long_name='midpoint of period'
-	outVar[:] = period_midpoints[0:per_num,:,:]
+		ds_out.state_file = state_file
 
-	outVar = nc_out.createVariable('period_monthly_index','i2',('period_id',lat_name,lon_name,))
-	outVar.long_name='monthly index 0 to number of years * 12'
-	outVar.first_time_step=str(year[0])+' - '+str(min(month))
-	outVar.last_time_step=str(year[-1])+' - '+str(max(month))
-	outVar[:] = period_monthly_index[0:per_num,:,:]
 
-	outVar = nc_out.createVariable('period_season','i1',('period_id',lat_name,lon_name,))
-	outVar.long_name='season in which the midpoint of period is located'
-	outVar.description=str(seasons)
-	outVar[:] = period_season[0:per_num,:,:]
-
-	nc_out.close()
-	nc_in.close()
+		ds_out.write_nc(state_file.replace('.nc','_period-'+state_name+'.nc'))
 
 def temp_anomaly_to_ind(anom_file,out_file,var_name='tas',seasons={'MAM':[3,4,5],'JJA':[6,7,8],'SON':[9,10,11],'DJF':[12,1,2]},overwrite=True):
 	"""
@@ -276,7 +306,6 @@ def temp_anomaly_to_ind(anom_file,out_file,var_name='tas',seasons={'MAM':[3,4,5]
 	if overwrite: os.system('rm '+out_file)
 	state.description='daily anomalies - seasonal medain of daily anomalies at grid cell level. positive anomalies -> 1 negative anomalies -> -1'
 	da.Dataset({'state':state}).write_nc(out_file)
-
 
 def precip_to_index_percentile(in_file,out_file,percentile_field,var_name='pr',percentile_multiplier=1, unit_multiplier=86400, overwrite=True):
 	"""
@@ -365,7 +394,6 @@ def precip_to_index(in_file,out_file,var_name='pr',unit_multiplier=1,threshold=0
 	if overwrite: os.system('rm '+out_file)
 	state.description='dry (wet) days are days with precipiation below (above) '+str(threshold)+' and are saved as -1 (1)'
 	da.Dataset({'state':state}).write_nc(out_file)
-
 
 def compound_precip_temp_index(tas_state_file,pr_state_file,out_file,overwrite=True):
 	"""
@@ -461,3 +489,116 @@ def compound_precip_temp_index(tas_state_file,pr_state_file,out_file,overwrite=T
 # 			cut_start=cut_stop+1
 #
 # 	return(pers)
+
+
+# def get_persistence_stable(state_file,out_file,states_to_analyze=[-1,1], lat_name='lat', lon_name='lon', seasons={'MAM':{'months':[3,4,5],'index':0}, 'JJA':{'months':[6,7,8],'index':1}, 'SON':{'months':[9,10,11],'index':2}, 'DJF':{'months':[12,1,2],'index':3}},overwrite=True):
+# 	"""
+# 	This function reads a state field created by :meth:`temp_anomaly_to_ind` or :meth:`precip_to_index` and finds persistent periods for these statesself. It uses :meth:`optimized_period_identifier`
+#
+# 	Parameters
+# 	----------
+# 		state_file: str
+# 			filepath of a state file. This file needs to have a variable `'state'` with -1 and 1 for the two different states. This file can be created by :meth:`temp_anomaly_to_ind` or :meth:`precip_to_index`
+# 		out_file: str
+# 			filepath of a period file
+# 		seasons: dict, default=`{'MAM':{'months':[3,4,5],'index':0}, 'JJA':{'months':[6,7,8],'index':1}, 'SON':{'months':[9,10,11],'index':2}, 'DJF':{'months':[12,1,2],'index':3}}``
+# 			dictionnary used to cluster detected periods into seasons. If no seasonal analysis is required use `seasons={'year':{'months':range(12),'index':0}}`
+# 		overwrite: bool
+# 			overwrites existing files
+# 	"""
+#
+# 	nc_in=Dataset(state_file,'r')
+# 	# handle time
+# 	time_axis=nc_in.variables['time'][:]
+# 	if 'calendar' in nc_in.variables['time'].ncattrs():
+# 		datevar = num2date(time_axis,units = nc_in.variables['time'].units,calendar = nc_in.variables['time'].calendar)
+# 	else:
+# 		datevar = num2date(time_axis,units = nc_in.variables['time'].units)
+#
+# 	month=np.array([int(str(date).split("-")[1])	for date in datevar[:]])
+# 	year=np.array([int(str(date).split("-")[0])	for date in datevar[:]])
+#
+# 	season=month.copy()*np.nan
+# 	for sea in seasons.keys():
+# 		season[np.where((month==seasons[sea]['months'][0]) | (month==seasons[sea]['months'][1]) | (month==seasons[sea]['months'][2]) )[0]]=seasons[sea]['index']
+#
+# 	monthly_index=np.array([mon+yr*12 for mon,yr in zip(month-1,year-np.min(year))])
+# 	mon_year_axis=np.array([yr+mn*0.01 for yr,mn in zip(year,month)])
+#
+# 	state=np.ma.getdata(nc_in.variables['state'][:].squeeze())
+# 	mask=np.ma.getmask(nc_in.variables['state'][:].squeeze())
+# 	state[mask]=np.nan
+# 	print(state.shape)
+# 	print(np.nanpercentile(state,range(100)))
+#
+#
+# 	#period=state.copy()*np.nan
+# 	period_length=state.copy()*np.nan
+# 	period_state=state.copy()*np.nan
+# 	period_midpoints=state.copy()*np.nan
+# 	period_season=state.copy()*np.nan
+# 	period_monthly_index=state.copy()*np.nan
+# 	gc.collect()
+#
+# 	period_number=[]
+# 	print('finding periods\n10------50-------100')
+# 	for y,progress in zip(range(state.shape[1]), np.array([['-']+['']*(state.shape[1]/20+1)]*20).flatten()[0:state.shape[1]]):
+# 		sys.stdout.write(progress); sys.stdout.flush()
+# 		for x in range(state.shape[2]):
+# 			start_time=time.time()
+# 			if np.isfinite(np.nanmean(state[:,y,x])) and np.nanmean(state[:,y,x]) not in [-1,1]:
+# 				periods=optimized_period_identifier(state[:,y,x].copy())
+# 				identified_periods=np.where(periods!=0)[0]
+# 				per_num=len(identified_periods)
+# 				period_number.append(per_num)
+#
+# 				period_length[0:per_num,y,x]=periods[identified_periods]
+# 				period_state[0:per_num,y,x]=np.sign(periods[identified_periods])
+# 				period_midpoints[0:per_num,y,x]=time_axis[identified_periods]
+# 				period_season[0:per_num,y,x]=season[identified_periods]
+# 				period_monthly_index[0:per_num,y,x]=monthly_index[identified_periods]
+# 			gc.collect()
+#
+# 	# if len(period_number)==0:
+# 	# 	return 'fail'
+#
+# 	per_num=max(period_number)
+#
+# 	if overwrite: os.system('rm '+out_file)
+# 	nc_out=Dataset(out_file,'w')
+# 	for dname, the_dim in nc_in.dimensions.iteritems():
+# 		if dname in [lon_name,lat_name]:nc_out.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
+# 	nc_out.createDimension('period_id', per_num)
+#
+# 	for v_name, varin in nc_in.variables.iteritems():
+# 		if v_name in [lon_name,lat_name]:
+# 			outVar = nc_out.createVariable(v_name, varin.datatype, varin.dimensions)
+# 			outVar.setncatts({k: varin.getncattr(k) for k in varin.ncattrs()})
+# 			outVar[:] = varin[:]
+#
+# 	outVar = nc_out.createVariable('period_length','i2',('period_id',lat_name,lon_name,))
+# 	outVar.long_name='period length in days'
+# 	outVar[:] = period_length[0:per_num,:,:]
+#
+# 	outVar = nc_out.createVariable('period_state','i1',('period_id',lat_name,lon_name,))
+# 	outVar.long_name='period type'
+# 	outVar.description=nc_in['state'].description
+# 	outVar[:] = period_state[0:per_num,:,:]
+#
+# 	outVar = nc_out.createVariable('period_midpoints','f',('period_id',lat_name,lon_name,))
+# 	outVar.long_name='midpoint of period'
+# 	outVar[:] = period_midpoints[0:per_num,:,:]
+#
+# 	outVar = nc_out.createVariable('period_monthly_index','i2',('period_id',lat_name,lon_name,))
+# 	outVar.long_name='monthly index 0 to number of years * 12'
+# 	outVar.first_time_step=str(year[0])+' - '+str(min(month))
+# 	outVar.last_time_step=str(year[-1])+' - '+str(max(month))
+# 	outVar[:] = period_monthly_index[0:per_num,:,:]
+#
+# 	outVar = nc_out.createVariable('period_season','i1',('period_id',lat_name,lon_name,))
+# 	outVar.long_name='season in which the midpoint of period is located'
+# 	outVar.description=str(seasons)
+# 	outVar[:] = period_season[0:per_num,:,:]
+#
+# 	nc_out.close()
+# 	nc_in.close()
